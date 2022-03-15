35cd1cc8d19dbfe7894841d4bfd9a066dda0f291
Daniel Alexander Dam <dani36i9@stud.dk>
2022-03-15 14:31:18 +0100

    added descriptions again

--- src/Adventure.java
+++ src/Adventure.java
@@ -37,15 +37,15 @@ public class Adventure {
 	
 	public void setupGameState() {
 		// init rooms
-		Room r1 = new Room("r1", "forest entrance");
-		Room r2 = new Room("r2", "green forest");
-		Room r3 = new Room("r3", "riverside");
-		Room r4 = new Room("r4", "entrance to the black forest");
-		Room r5 = new Room("r5", "Treasure Room");
-		Room r6 = new Room("r6", "outer part of the black forest");
-		Room r7 = new Room("r7", "entrance to the swamp");
-		Room r8 = new Room("r8", "swamp");
-		Room r9 = new Room("r9", "abandoned cabin");
+		Room r1 = new Room("r1 Forest entrance", " you are at the entrance to the forest, the green trees bit you welcome");
+		Room r2 = new Room("r2 The green forest", "You continue through the entrance to the green forest, and are now deep inside the green forest");
+		Room r3 = new Room("r3 the outer part of the green forest", "The green forest is beginning to look a litte black, and you starts to wonder, what is ahead?");
+		Room r4 = new Room("r4 the entrance to the black forest", "You have just stepped through the entrance to the black forest, everything is black and dark. It is hard to see anything...");
+		Room r5 = new Room("r5 Welcome to the Treasure Room", ""); //TODO når der implimenteres våben og fjender kan vi lave noget sjovt med treasure room, indtil da lader jeg den være
+		Room r6 = new Room("r6 Outer part of the black forest", "As you continue you notice that there is not as many black trees as there used to be. you something in the distant...");
+		Room r7 = new Room("r7 Entrance to the swamp", "You have just left the Black forest and are now and the entrance to the swamp, the smell is weird and you begin to feel a little anxious.");
+		Room r8 = new Room("r8 Deep swamp", "You have walked inside the swamp for a bit, the strong smell keeps increasing and you are starting to feel a little lightweight");
+		Room r9 = new Room("r9 Dark cliffside", "you are hearing noises all around you, but can you can't tell which direction they are coming from.");
 		currentRoom = r1;
 		
 		// Connect rooms (both ways)
@@ -87,8 +87,8 @@ public class Adventure {
 				// Movement choices
 				case "n", "north", "go north" -> currentRoom = selectDirection(currentRoom, currentRoom.getNorth());
 				case "s", "south", "go south" -> currentRoom = selectDirection(currentRoom, currentRoom.getSouth());
-				case "w", "west", "go west" -> currentRoom = selectDirection(currentRoom, currentRoom.getWest());
-				case "e", "east", "go east" -> currentRoom = selectDirection(currentRoom, currentRoom.getEast());
+				case "w", "west", "go west" 	-> currentRoom = selectDirection(currentRoom, currentRoom.getWest());
+				case "e", "east", "go east" 	-> currentRoom = selectDirection(currentRoom, currentRoom.getEast());
 				// Other actions(also always available)
 				case "h", "help" -> printControls();
 				case "l", "look" -> {
